e009726e0f1f8db68848eac22db8da989dd6f083
Philemon Selvaraj <phil.lloyds@gmail.com>
2015-06-18 22:51:31 +0000

    Add complete authentication logic

--- app/actions.rb
+++ app/actions.rb
@@ -5,10 +5,49 @@
 #   erb :index#, :locals => {:client_id => "ec929278fb87047f1280"}
 # end
 
+
+# def helpers
+
+  # def new_user
+  #   client = Octokit::Client.new(:access_token => session[:access_token])
+
+
+  #   user = client.user
+    
+  #   @user = User.create
+  #   (
+  #     username: user.login,
+  #     token: session[:access_token],
+  #     avatar_url: user.avatar_url,
+  #     location: user.location,
+  #     followers: user.followers,
+  #     following: user.following,
+  #     public_repos: user.public_repos,
+  #     public_gists: user.public_gists,
+  #     start_date: user.created_at
+  #   )
+
+  # end
+
+# end
+
+def current_user
+  User.find(session[:user_id]) if session[:user_id]
+end
+
+
 get "/" do
-  gh_data = get_github_data()
+  # gh_data = get_github_data()
   # Pass in the CLIENT_ID for the login button on the home page.
-  erb :"index", :locals => {:client_id => CLIENT_ID, :gh_data => gh_data}
+  # @user = current_user
+  
+  erb :index
+
+end
+
+get "/me" do
+  @user = current_user
+  json @user
 end
 
 get '/login' do
@@ -16,75 +55,48 @@ get '/login' do
   redirect "https://github.com/login/oauth/authorize?scope=user&client_id=#{client_id}"
 end
 
-# get '/callback' do
-#   # get temporary GitHub code...
-#   session_code = request.env['rack.request.query_hash']['code']
-
-#   # ... and POST it back to GitHub
-#   result = RestClient.post('https://github.com/login/oauth/access_token',
-#                           {
-#                             client_id: ENV['CLIENT_ID'] || 'ec929278fb87047f1280',
-#                             client_secret: ENV['CLIENT_SECRET'] || '2f6d93489179df343c2fc243d0a1aafd471d556e',
-#                             code: session_code,
-#                           },
-#                           accept: :json
-#                           )
-
-#   # extract the token and granted scopes
-#   session[:access_token] = JSON.parse(result)['access_token']
-#   client = Octokit::Client.new(:access_token => session[:access_token])
-
-# @user = User.create(
-#     username: client.user,
-#     token: session[:access_token],
-#     avatar_url: client.user.avatar_url,
-#     location: client.user.location,
-#     followers: client.user.followers,
-#     following: client.user.following,
-#     public_repos: client.user.public_repos,
-#     public_gists: client.user.public_gists,
-#     start_date: client.user.created_at)
-
-#   redirect '/'
-# end
 
 get '/callback' do
   # Get temporary GitHub code...
   session_code = request.env['rack.request.query_hash']['code']
  
-  # ... and POST it back to GitHub
   result = RestClient.post('https://github.com/login/oauth/access_token',
-                          {:client_id => CLIENT_ID,
-                           :client_secret => CLIENT_SECRET,
-                           :code => session_code},
-                           :accept => :json)
-  # example result:
-  # { "access_token":"xxasdfasdf234234123dvadsfasdfas",
-  #   "token_type":"bearer",
-  #   "scope":"user:email"
-  # }
- 
+    {
+      client_id: ENV['CLIENT_ID'] || 'ec929278fb87047f1280',
+      client_secret: ENV['CLIENT_SECRET'] || '2f6d93489179df343c2fc243d0a1aafd471d556e',
+      code: session_code,
+    },
+    accept: :json
+  ) 
   # Make the access token available across sessions.
-  session[:access_token] = JSON.parse(result)['access_token']
+  token = JSON.parse(result)['access_token']
 
-  # As soon as someone authenticates, we kick them to the home page.
+  data = Octokit::Client.new(access_token: token).user
 
-  client = Octokit::Client.new(:access_token => session[:access_token])
+  user = User.find_by(username: data.username)
 
-@user = User.create(
-    username: client.user.login,
-    token: session[:access_token],
-    avatar_url: client.user.avatar_url,
-    location: client.user.location,
-    followers: client.user.followers,
-    following: client.user.following,
-    public_repos: client.user.public_repos,
-    public_gists: client.user.public_gists,
-    start_date: client.user.created_at)
+  unless user then
+    user = User.create(
+      username: data.login,
+      token: token,
+      avatar_url: data.avatar_url,
+      location: data.location,
+      followers: data.followers,
+      following: data.following,
+      public_repos: data.public_repos,
+      public_gists: data.public_gists,
+      start_date: data.created_at
+    )
+  end
 
-  redirect '/'
+  session[:user_id] = user.id
+  erb :login
 end
 
+get '/logout' do
+  session.clear
+  redirect '/'
+end
 
 
 
