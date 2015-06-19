e8ca5d341e2fc6969ad00ba7229531b46ce29aa0
bhammett <beth.hammett@gmail.com>
2015-06-19 21:54:08 +0000

    achievement table population

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150619191929) do
+ActiveRecord::Schema.define(version: 20150619191935) do
 
   create_table "achievements", force: :cascade do |t|
     t.string   "name"
-    t.string   "level"
+    t.integer  "level"
     t.string   "url"
     t.integer  "criteria"
     t.datetime "created_at", null: false
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20150619191929) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "token"
     t.string   "avatar_url"
     t.string   "location"
