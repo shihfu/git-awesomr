49c0787429cbb0ffd65c469c99c2e1bf28b44b18
Philemon Selvaraj <phil.lloyds@gmail.com>
2015-06-21 02:39:55 +0000

    Add logic to actions.rb

--- app/actions.rb
+++ app/actions.rb
@@ -31,9 +31,220 @@ def authenticate!
   redirect url
 end
 
+
+def account_open
+  @days_open = (Date.today - @user.start_date).to_i 
+  years_open = @days_open/365.to_f 
+  achievement_name = @achievement.where(name: "Account Open") 
+  achievements = achievement_name.select {|a| a.criteria <= years_open} 
+  @progress_bar_acc_open = @days_open/1095.to_f*100 
+
+  if achievements[0] 
+    @open_ach_url_1 = achievements[0][:url]
+    @total_flags +=1
+  end
+
+  if achievements[1] 
+    @open_ach_url_2 = achievements[1][:url]
+    @total_flags +=1
+  end
+
+  if achievements[2] 
+    @open_ach_url_3 = achievements[2][:url]
+    @total_flags +=1
+  end
+
+end
+
+def language_achieved
+  @lang_all=[]
+  @user.repos.each do |repo| 
+    @lang_all << repo.language 
+    @lang_all.compact!
+  end 
+
+  @lang_all.uniq!
+  achievement_name =[]
+  achievement_name = @achievement.where name: "Language"
+  achievements = achievement_name.select{|level| level.criteria <=(@lang_all.count) } 
+  @progress_bar_lang = @lang_all.count/achievement_name[2][:criteria].to_f*100
+
+ if achievements[0] 
+    @lang_ach_url_1 = achievements[0][:url]if achievements[0]
+    @total_flags +=1
+  end
+
+  if achievements[1] 
+    @lang_ach_url_2 = achievements[1][:url] if achievements[1]
+    @total_flags +=1
+  end
+  
+  if achievements[2] 
+    @lang_ach_url_3 = achievements[2][:url] if achievements[2]
+    @total_flags +=1
+  end
+
+end
+
+def followers_achieved
+  followers = @user.followers
+  achievement_name = @achievement.where name: "Followers"
+  achievements = []
+  achievements = achievement_name.select {|a| a.criteria <= followers}
+
+  if achievements[0] 
+    @follow_ach_url_1 = achievements[0][:url] if achievements[0]
+    @total_flags +=1 
+  end
+
+  if achievements[1] 
+    @follow_ach_url_2 = achievements[1][:url] if achievements[1]
+    @total_flags +=1
+  end
+
+  if achievements[2] 
+    @follow_ach_url_3 = achievements[2][:url] if achievements[2]
+    @total_flags +=1
+  end
+
+  followers_criteria = 0
+  followers_criteria = achievement_name[2][:criteria]
+  @progress_bar_followers = followers/followers_criteria.to_f*100 
+end
+
+def repositories_achieved
+  respositories = @user.public_repos
+  achievement_name = @achievement.where name: "Repos"
+  achievements = []
+  achievements = achievement_name.select {|a| a.criteria <= respositories}
+
+  if achievements[0] 
+    @repos_ach_url_1 = achievements[0][:url] if achievements[0]
+     @total_flags +=1 
+  end
+
+  if achievements[1] 
+    @repos_ach_url_2 = achievements[1][:url] if achievements[1]
+     @total_flags +=1 
+  end
+
+  if achievements[2] 
+    @repos_ach_url_3 = achievements[2][:url] if achievements[2]
+     @total_flags +=1 
+  end
+
+  repos_criteria = 0
+  repos_criteria = achievement_name[2][:criteria]
+  @progress_bar_repos = respositories/repos_criteria.to_f*100 
+end
+
+
+def forks_achieved
+ @forks_all = 0
+ @user.repos.each do |repo| 
+ @forks_all += repo.forks_count 
+ end 
+
+achievement_name = @achievement.where name: "Forks"
+achievements = []
+achievements = achievement_name.select {|a| a.criteria <= @forks_all}
+
+if achievements[0] 
+  @forks_ach_url_1 = achievements[0][:url] if achievements[0]
+  @total_flags +=1 
+end
+
+if achievements[1] 
+  @forks_ach_url_2 = achievements[1][:url] if achievements[1]
+   @total_flags +=1 
+end
+
+if achievements[2] 
+  @forks_ach_url_3 = achievements[2][:url] if achievements[2]
+  @total_flags +=1 
+end
+
+forks_criteria = 0
+forks_criteria = achievement_name[2][:criteria]
+
+@progress_bar_forks =@forks_all/forks_criteria.to_f*100 
+end
+
+
+def commits_achieved
+  days_open = (Date.today - @user.start_date).to_i 
+  @commits_all = 0
+  @user.repos.each do |repo| 
+  @commits_all += repo.commits_count 
+  end 
+
+  achievement_name = @achievement.where name: "Commits"
+  achievements = []
+  achievements = achievement_name.select {|a| a.criteria <= @commits_all}
+
+  if achievements[0]
+   @commits_ach_url_1 = achievements[0][:url] if achievements[0]
+   @total_flags +=1 
+  end
+
+  if achievements[1]
+    @commits_ach_url_2 = achievements[1][:url] if achievements[1]
+    @total_flags +=1 
+  end
+
+  if achievements[2]
+    @commits_ach_url_3 = achievements[2][:url] if achievements[2]
+    @total_flags +=1 
+  end
+
+  commits_criteria = 0
+  commits_criteria = achievement_name[2][:criteria]
+
+  @progress_bar_commits = @commits_all/commits_criteria.to_f*100 
+end
+
+def stars_achieved
+  @stars_all = 0
+  @user.repos.each do |repo| 
+  @stars_all += repo.stars_count 
+  end 
+
+  achievement_name = @achievement.where name: "Stars"
+  achievements = []
+  achievements = achievement_name.select {|a| a.criteria <= @stars_all}
+
+  if achievements[0]
+    @stars_ach_url_1 = achievements[0][:url] if achievements[0]
+    @total_flags +=1 
+  end
+
+  if achievements[1]
+    @stars_ach_url_2 = achievements[1][:url] if achievements[1]
+    @total_flags +=1 
+  end
+
+  if achievements[2]
+    @stars_ach_url_3 = achievements[2][:url] if achievements[2]
+    @total_flags +=1 
+  end
+
+  stars_criteria = 0
+  stars_criteria = achievement_name[2][:criteria]
+
+  @progress_bar_stars = @stars_all/stars_criteria.to_f*100 
+end
+
 get '/user/:username' do
+  @total_flags = 0 
   @user = current_user
   @achievement = achievements
+  account_open
+  language_achieved
+  followers_achieved
+  repositories_achieved
+  forks_achieved
+  commits_achieved
+  stars_achieved
   erb :'user/index'
 end
 
