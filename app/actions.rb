a28d881c771ac3a7b90fd1400827504575e9ce8d
Philemon Selvaraj <phil.lloyds@gmail.com>
2015-06-21 19:34:48 +0000

    Add logic for search users

--- app/actions.rb
+++ app/actions.rb
@@ -8,165 +8,16 @@ CLIENT_SECRET = '2f6d93489179df343c2fc243d0a1aafd471d556e'
 
 use Rack::Session::Cookie, :secret => rand.to_s()
 
-get "/" do
-  erb :index
-end
-
-get '/login' do
-  if !authenticated?
-    authenticate!
-  else
-    access_token = session[:access_token]
-    scopes = []
-
-    client = Octokit::Client.new \
-      :client_id => CLIENT_ID,
-      :client_secret => CLIENT_SECRET
-
-    begin
-      client.check_application_authorization access_token
-    rescue => e
-      # request didn't succeed because the token was revoked so we
-      # invalidate the token stored in the session and render the
-      # index page so that the user can start the OAuth flow again
-      session[:access_token] = nil
-      return authenticate!
-    end
-
-    client = Octokit::Client.new :access_token => access_token
-    data = client.user
-    user = User.find_by(username: data.login)
-    session[:user_id] = user.id
-    redirect "/user/#{user.username}"
-end
-end
-
-get '/callback' do
-  session_code = request.env['rack.request.query_hash']['code']
-  result = Octokit.exchange_code_for_token(session_code, CLIENT_ID, CLIENT_SECRET)
-  session[:access_token] = result[:access_token]
-  
-  @total_flags = 0 
-  @user = current_user
-  @achievement = achievements
-  client = Octokit::Client.new :access_token => session[:access_token]
-  data = client.user
-  user = User.find_by(username: data.login)
-  @user_repos = Octokit.repositories data.login
-
-  unless user then
-    user = User.create(
-      username: data.login,
-      first_name: user_full_name[0],
-      last_name: user_full_name[1],
-      token: session[:access_token],
-      avatar_url: data.avatar_url,
-      location: data.location,
-      followers: data.followers,
-      public_repos: data.public_repos,
-      public_gists: data.public_gists,
-      start_date: data.created_at
-    )
-
-    user_repos.each do |repo|
-      commit_activity = Octokit.participation_stats(data.login+"/"+repo.name)
-      total_commits += commit_activity[:owner].inject(0){|total,week| total+week} if commit_activity
-
-      user_achievement_info = Repo.create(
-      name: repo.name,
-      user_id: user.id,
-      stars_count: repo.stargazers_count,
-      forks_count: repo.forks_count,
-      commits_count: total_commits,
-      language: repo.language
-      )
-      total_commits = 0
-    end
-end
-
-  erb :'user/index'
-
-  session[:user_id] = user.id
-
-  redirect "/user/#{user.username}"
-end
-
-
-get '/user/:username' do
-  octokit_login
-  account_open
-  language_achieved
-  followers_achieved
-  repositories_achieved
-  forks_achieved
-  commits_achieved
-  stars_achieved
-  @user.update flags: @total_flags
-  @score = (@total_flags/21.to_f*100).to_i
-  @user.update score: @score
-end
-
-
-get '/logout' do
-  session.clear
-  redirect '/'
-end
-
-get '/user' do
-  erb :'user/index'
-end
-
-get '/id' do
-  @user = current_user
-  erb :'/users/id'
-end
-
-# get '/group' do
-#   erb :'group/new'
-# end
-
-# post '/group' do
-#   name = params[:name]
-#   username = params[:username]
-
-#   group = Group.find_by(name: name)
-#     if group
-#       redirect '/group/:id'
-#     else
-#       group = Group.create(name: name)
-#       redirect '/group/:id'
-#     end
-# end
-
-post '/search' do
-  search_name = params[:searchname]
-  create_user
-  erb :'user/index'
-  session[:user_id] = user.id
-  redirect "/user/#{user.username}"
-end
-
-get '/group' do
-  # GRAPH DATA
-  @achievement = {}
-  @achievement[:user] = [1, 2, 3, 2, 2, 2, 3]
-  @achievement[:group] = [2, 1, 2, 3, 2, 3, 1]
-
-
-  @average_score = {}
-  @average_score[:user] = [10, 20, 30, 20, 20, 20, 30, 30]
-  @average_score[:group] = [20, 20, 20, 20, 20, 20, 20, 20]
-  erb :'group/index'
-end
-
-
-
 def current_user
   User.find(session[:user_id]) if session[:user_id]
 end
 
 def achievements
   Achievement.all
+end
+
+get "/" do
+  erb :index
 end
 
 def authenticated?
@@ -183,10 +34,11 @@ end
 
 def account_open
   @days_open = (Date.today - @user.start_date).to_i 
-  years_open = @days_open/365.to_f 
+  @years_open = @days_open/365.to_f 
   achievement_name = @achievement.where(name: "Account Open") 
-  achievements = achievement_name.select {|a| a.criteria <= years_open} 
-  @progress_bar_acc_open = @days_open/1095.to_f*100 
+  achievements = achievement_name.select {|a| a.criteria <= @years_open} 
+  @progress_bar_acc_open = @days_open/1095.to_f*100
+  # @progress_bar_acc_open = [@progress_bar_acc_open,@max_progress_bar].min
 
   if achievements[0] 
     @open_ach_url_1 = achievements[0][:url]
@@ -383,14 +235,55 @@ def stars_achieved
   @progress_bar_stars = @stars_all/stars_criteria.to_f*100 
 end
 
-def create_user
- 
- octokit_login
- 
-  
+post '/search' do
+  searchname = params[:searchname]
+  @max_progress_bar = 100
+  # @user = User.find_by(username: searchname)
+  client = Octokit::Client.new(:login => 'philemonlloyds', :password => 'Binitha@1088')
+  @search_user = Octokit.user searchname
+  user_full_name = @search_user[:name].split
+
+  search_user = User.create(
+      username: searchname,
+      first_name: user_full_name[0],
+      last_name: user_full_name[1],
+      avatar_url: @search_user[:avatar_url],
+      location: @search_user[:location],
+      followers: @search_user[:followers],
+      public_repos: @search_user[:public_repos],
+      public_gists: @search_user[:public_gists],
+      start_date: @search_user[:created_at]
+    ) 
+
+  @user_repos = @search_user.rels[:repos].get.data
+
+  @user_repos.each do |repo|
+      user_achievement_info = Repo.create(
+      name: repo.name,
+      user_id: search_user.id,
+      stars_count: repo.stargazers_count,
+      forks_count: repo.forks_count,
+      # commits_count: total_commits,
+      language: repo.language
+      )
+      total_commits = 0
+    end
+
+  session[:user_id] = search_user.id
+  @total_flags = 0 
+  @user = search_user
+  @achievement = achievements
+  account_open
+  language_achieved
+  followers_achieved
+  repositories_achieved
+  forks_achieved
+  stars_achieved
+  @user.update(score: @total_flags/21.to_f*100.to_i)
+  erb :'user/index'
 end
 
-def octokit_login
+get '/user/:username' do
   @total_flags = 0 
   @user = current_user
   @achievement = achievements
@@ -398,5 +291,134 @@ def octokit_login
   data = client.user
   user = User.find_by(username: data.login)
   @user_repos = Octokit.repositories data.login
+  account_open
+  language_achieved
+  followers_achieved
+  repositories_achieved
+  forks_achieved
+  commits_achieved
+  stars_achieved
+  user.update(score: @total_flags/21.to_f*100.to_i)
+  erb :'user/index'
 end
 
+get '/login' do
+  if !authenticated?
+    authenticate!
+  else
+    access_token = session[:access_token]
+    scopes = []
+
+    client = Octokit::Client.new \
+      :client_id => CLIENT_ID,
+      :client_secret => CLIENT_SECRET
+
+    begin
+      client.check_application_authorization access_token
+    rescue => e
+      # request didn't succeed because the token was revoked so we
+      # invalidate the token stored in the session and render the
+      # index page so that the user can start the OAuth flow again
+      session[:access_token] = nil
+      return authenticate!
+    end
+
+    client = Octokit::Client.new :access_token => access_token
+    data = client.user
+    user = User.find_by(username: data.login)
+    session[:user_id] = user.id
+    redirect "/user/#{user.username}"
+end
+end
+
+get '/callback' do
+  session_code = request.env['rack.request.query_hash']['code']
+  result = Octokit.exchange_code_for_token(session_code, CLIENT_ID, CLIENT_SECRET)
+  session[:access_token] = result[:access_token]
+
+  client = Octokit::Client.new :access_token => session[:access_token]
+  data = client.user
+  user = User.find_by(username: data.login)
+  user_repos = Octokit.repositories data.login
+  user_full_name = data.name.split
+  total_commits = 0
+
+  unless user then
+    user = User.create(
+      username: data.login,
+      first_name: user_full_name[0],
+      last_name: user_full_name[1],
+      token: session[:access_token],
+      avatar_url: data.avatar_url,
+      location: data.location,
+      followers: data.followers,
+      public_repos: data.public_repos,
+      public_gists: data.public_gists,
+      start_date: data.created_at
+    )
+
+    user_repos.each do |repo|
+      commit_activity = Octokit.participation_stats(data.login+"/"+repo.name)
+      total_commits += commit_activity[:owner].inject(0){|total,week| total+week} if commit_activity
+
+      user_achievement_info = Repo.create(
+      name: repo.name,
+      user_id: user.id,
+      stars_count: repo.stargazers_count,
+      forks_count: repo.forks_count,
+      commits_count: total_commits,
+      language: repo.language
+      )
+      total_commits = 0
+    end
+ end
+
+  session[:user_id] = user.id
+
+  redirect "/user/#{user.username}"
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
+
+get '/user' do
+  erb :'user/index'
+end
+
+get '/id' do
+  @user = current_user
+  erb :'/users/id'
+end
+
+# get '/group' do
+#   erb :'group/new'
+# end
+
+# post '/group' do
+#   name = params[:name]
+#   username = params[:username]
+
+#   group = Group.find_by(name: name)
+#     if group
+#       redirect '/group/:id'
+#     else
+#       group = Group.create(name: name)
+#       redirect '/group/:id'
+#     end
+# end
+
+get '/group' do
+  # GRAPH DATA
+
+  @achievement = {}
+  @achievement[:user] = [@user.acct_age_level, @user.languages_level, @user.followers_level, @user.repos_level, @user.forks_level, @user.commits_level, @user.stars_level]
+  @achievement[:group] = [2, 1, 2, 3, 2, 3, 1]
+
+
+  @average_score = {}
+  @average_score[:user] = [10, 20, 30, 20, 20, 20, 30, 30]
+  @average_score[:group] = [20, 20, 20, 20, 20, 20, 20, 20]
+  erb :'group/index'
+end
